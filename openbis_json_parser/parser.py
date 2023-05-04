3c0fbb12938a7f1c1ce83ac42deb8b44f9ad132b
Hanke <hanke@iwm.fraunhofer.de>
2023-05-04 17:16:36 +0200

    first working, permId are not related

--- openbis_json_parser/parser.py
+++ openbis_json_parser/parser.py
@@ -3,161 +3,128 @@ import json
 import tempfile
 import pathlib
 
-import owlready2 as owl
-import rdflib
+from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib.namespace import RDF, RDFS, XSD, OWL
 
-
+ns=Namespace('http://example.com/')
 def load_ontology():
     with tempfile.TemporaryDirectory() as tmpdir:
-        g = rdflib.Graph()
+        g = Graph()
         g.parse(str(pathlib.Path(__file__).parent.parent / 'openbis.ttl'))
-        g.serialize(format='ntriples', destination=pathlib.Path(tmpdir, 'openbis.nt'), encoding='utf-8')
-        onto = owl.get_ontology(str(pathlib.Path(tmpdir, 'openbis.nt'))).load()
-    return onto
+    return g
 
+OBIS=Namespace('https://purl.matolab.org/openbis/')
+obis=load_ontology()
+
+def get_obis_entity(string: str):
+    hits=list(obis[:OBIS.openbis_json_key:Literal(string)])
+    if hits:
+        return hits[0]
+    else:
+        return None
+
+def get_custom_props(string: str, graph):
+    hits=list(graph[:OBIS.code:Literal(string)])
+    if hits:
+        return hits[0]
+    else:
+        return None
 
 def write_ontology(onto, target_file, target_format):
     if target_format in ('ntriples', 'nquads', 'rdfxml'):
         onto.save(target_file, format=target_format)
     elif target_format in ('turtle', 'ttl', 'json-ld'):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            onto.save(str(pathlib.Path(tmpdir, 'openbis.nt')), format='ntriples')
-            g = rdflib.Graph()
-            g.parse(str(pathlib.Path(tmpdir, 'openbis.nt')))
         if isinstance(target_file, str):
             with open(target_file, 'wb') as f:
-                f.write(g.serialize(format=target_format).encode('utf-8'))
+                f.write(onto.serialize(format=target_format).encode('utf-8'))
         else:
-            target_file.write(g.serialize(format=target_format).encode('utf-8'))
-
-
-def _find_or_create(instance, cls, data):
-    if data is None:
-        return None
-    obj = next(filter(lambda e: e.code == data['code'], instance.namespace.get_instances_of(cls)), None)
-    if obj is None:
-        obj = add_data(instance, data)
-    return obj
-
-
-def add_sample(instance, data):
-    onto = instance.namespace
-    sample = onto.Object()
-    sample.permID = data['permId']['permId']
-    sample.identifier = data['identifier']['identifier']
-    sample.code = data.get('code')
-    sample.project = _find_or_create(instance, onto.Project, data.get('project'))
-    sample.space = _find_or_create(instance, onto.Space, data.get('space'))
-    sample.type = _find_or_create(instance, onto.ObjectType, data.get('type'))
-    for p_code, p_value in data.get('properties', {}).items():
-        p = onto.Property()
-        p.type = next(filter(lambda e: e.code == p_code, onto.get_instances_of(onto.PropertyType)), None)
-        if p.type is None:
-            raise Exception(f'invalid property type {p_type}')
-        match p.type.dataType:
-            case 'TIMESTAMP':
-                p.propertyValue = datetime.datetime.strptime(p_value, '%Y-%m-%d %H:%M:%S +0000')
-            case 'VARCHAR':
-                p.propertyValue = p_value
-            case 'REAL':
-                p.propertyValue = float(p_value)
-            case _:
-                raise Exception(f'invalid data type: {p.type.dataType}')
-        sample.properties.append(p)
-    return sample
-
-
-def add_sample_type(instance, data):
-    onto = instance.namespace
-    sample_type = onto.ObjectType()
-    sample_type.code = data.get('code')
-    sample_type.description = data.get('description')
-    for pa in data.get('propertyAssignments', []):
-        sample_type.propertyAssignments.append(add_property_assignment(instance, pa))
-    return sample_type
-
-
-def add_property_assignment(instance, data):
-    onto = instance.namespace
-    pa = onto.PropertyAssignment()
-    pa.ordinal = data.get('ordinal')
-    pa.mandatory = data.get('mandatory')
-    pa.type = _find_or_create(instance, onto.PropertyType, data.get('propertyType'))
-    pa.registrationDate = datetime.datetime.fromtimestamp(data['registrationDate'] / 1000)
-    return pa
-
-
-def add_property_type(instance, data):
-    onto = instance.namespace
-    pt = onto.PropertyType()
-    pt.code = data.get('code')
-    pt.label = data.get('label')
-    pt.description = data.get('description')
-    pt.dataType = data.get('dataType')
-    return pt
-
-
-def add_project(instance, data):
-    onto = instance.namespace
-    project = onto.Project()
-    project.permID = data['permId']['permId']
-    project.identifier = data['identifier']['identifier']
-    project.code = data.get('code')
-    project.description = data.get('description')
-    project.registrationDate = datetime.datetime.fromtimestamp(data['registrationDate'] / 1000)
-    project.space = _find_or_create(instance, onto.Space, data.get('space'))
-    return project
-
-
-def add_collection(instance, data):
-    onto = instance.namespace
-    collection = onto.Collection()
-    collection.permId = data['permId']['permId']
-    collection.identifier = data['identifier']['identifier']
-    collection.code = data.get('code')
-    collection.registrationDate = datetime.datetime.fromtimestamp(data['registrationDate'] / 1000)
-    return collection
-
-
-def add_space(instance, data):
-    onto = instance.namespace
-    space = onto.Space()
-    space.permID = data['permId']['permId']
-    space.code = data.get('code')
-    space.description = data.get('description')
-    space.registrationDate = datetime.datetime.fromtimestamp(data['registrationDate'] / 1000)
-    return space
-
-
-def add_data(instance, data):
-    match data.get('@type'):
-        case 'as.dto.sample.Sample':
-            return add_sample(instance, data)
-        case 'as.dto.sample.SampleType':
-            return add_sample_type(instance, data)
-        case 'as.dto.property.PropertyAssignment':
-            return add_property_assignment(instance, data)
-        case 'as.dto.property.PropertyType':
-            return add_property_type(instance, data)
-        case 'as.dto.experiment.Experiment':
-            return add_collection(instance, data)
-        case 'as.dto.project.Project':
-            return add_project(instance, data)
-        case 'as.dto.space.Space':
-            return add_space(instance, data)
-        case _:
-            raise Exception(f'invalid data type: {data.get("@type")}')
+            target_file.write(onto.serialize(format=target_format).encode('utf-8'))
 
 
 def parse_dict(data):
-    onto = load_ontology()
-    instance = onto.Instance()
-    for entity in data.values():
-        add_data(instance, entity)
-    return onto
+    result = Graph()
+    iterate_json(data,result)
+    result=fix_iris(result)
+    return result
 
 
 def parse_json(file_path):
     with open(file_path) as f:
         data = json.load(f)
     return parse_dict(data)
+
+def create_instance(data: dict):
+    if all(prop in data.keys() for prop in ['@id','@type']):
+        id=str(data['@id'])
+        o_class=get_obis_entity(data['@type'])
+        if o_class:
+            return ns[id],o_class
+        else:
+            return None, None  
+    else:
+        return None, None  
+        
+
+def iterate_json(data, graph):
+    if isinstance(data, dict):
+        entity, e_class = create_instance(data)
+        if entity and e_class:
+            graph.add((entity,RDF.type,e_class))
+           
+        for key, value in data.items():
+            if key=="properties" and isinstance(value,dict):
+                # lookup in graph 
+                for prop_key, prop_value in value.items():
+                    obj_prop=get_custom_props(prop_key,graph)
+                    if obj_prop:
+                        #print(prop_key,obj_prop,prop_value)
+                        graph.add((entity,obj_prop,Literal(str(prop_value))))
+            elif isinstance(value, (dict, list)):
+                iterate_json(value,graph)
+            else:
+                #print(key)
+                annotation=get_obis_entity(key)
+                if not value:
+                    continue
+                elif entity and annotation and key in ['registrationDate','modificationDate']:
+                    timestamp = value / 1000  # convert milliseconds to seconds
+                    dt = datetime.datetime.fromtimestamp(timestamp)
+                    iso_string = dt.isoformat()
+                    graph.add((entity,annotation,Literal(str(iso_string))))
+                elif entity and annotation and key in ['project','space','experiment']:
+                    #print(value,type(value))
+                    graph.add((entity,annotation,ns[str(value)]))
+                    
+                elif entity and annotation and isinstance(value,str):
+                    graph.add((entity,annotation,Literal(value)))
+                    #print(key, ":", value, annotation)
+                pass
+    elif isinstance(data, list):
+        for item in data:
+            iterate_json(item,graph)
+
+def replace_iris(old: URIRef, new: URIRef, graph: Graph):
+    ### replaces al iri of all tripple in a graph with the value of relation
+    old_triples = list(graph[old: None: None])
+    for triple in old_triples:
+        graph.remove((old, triple[0], triple[1]))
+        graph.add((new, triple[0], triple[1]))
+    old_triples = list(graph[None : None: old])
+    for triple in old_triples:
+        graph.remove((triple[0], triple[1],old))
+        graph.add((triple[0], triple[1], new))
+    old_triples = list(graph[None : old : None])
+    for triple in old_triples:
+        graph.remove((triple[0] ,old, triple[1]))
+        graph.add((triple[0], new, triple[1]))
+
+def fix_iris(graph):
+    # replaces the iris of entties by attached values of code or permId
+    for entity_uri, _, value in graph.triples((None, OBIS.code, None)):
+        new=ns[value]        
+        replace_iris(entity_uri,new,graph)
+    # if the have a permId better use that
+    for entity_uri, _, value in graph.triples((None, OBIS.code, None)):
+    
+    #replace_iris(ns,OBIS.code,graph)
+    return graph

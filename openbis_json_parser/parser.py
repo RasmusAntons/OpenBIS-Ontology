296c3c9c06e2faf57942af99e74480c31ef86654
Rasmus Antons <rasmus.antons@iwm.fraunhofer.de>
2023-05-31 15:43:15 +0200

    add base_url parameter

--- openbis_json_parser/parser.py
+++ openbis_json_parser/parser.py
@@ -2,23 +2,29 @@ import datetime
 import json
 import pathlib
 import tempfile
+import urllib.parse
 
 from rdflib import Graph, Namespace, URIRef, Literal
 from rdflib.namespace import RDF, XSD, OWL
 
 # plugin api endpoint for permIds should be here
-ns = Namespace('https://openbis.matolab.org/openbismantic/')
+default_ns = Namespace('https://openbis.matolab.org/openbismantic/')
 
 
 def load_ontology():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        g = Graph()
-        g.parse(str(pathlib.Path(__file__).parent.parent / 'openbis.ttl'))
+    g = Graph()
+    g.parse(str(pathlib.Path(__file__).parent.parent / 'openbis.ttl'))
     return g
 
 
 OBIS = Namespace('https://purl.matolab.org/openbis/')
 obis = load_ontology()
+
+
+def _get_ns(base_url=None):
+    if base_url is None:
+        return default_ns
+    return Namespace(urllib.parse.urljoin(base_url, 'openbismantic/'))
 
 
 def get_obis_entity(string: str):
@@ -45,37 +51,37 @@ def write_ontology(onto, target_file, target_format):
         target_file.write(onto.serialize(format=target_format).encode('utf-8'))
 
 
-def parse_dict(data):
+def parse_dict(data, base_url=None):
     result = Graph()
     result.bind('obis', OBIS)
-    result.bind('data', ns)
-    iterate_json(data, result)
-    result = fix_iris(result)
+    result.bind('data', _get_ns(base_url))
+    iterate_json(data, result, base_url=base_url)
+    result = fix_iris(result, base_url=base_url)
     return result
 
 
-def parse_json(file_path):
+def parse_json(file_path, base_url=None):
     with open(file_path) as f:
         data = json.load(f)
-    return parse_dict(data)
+    return parse_dict(data, base_url=base_url)
 
 
-def create_instance_triple(data: dict):
+def create_instance_triple(data: dict, base_url=None):
     if all(prop in data.keys() for prop in ['@id', '@type']):
         instance_id = str(data['@id'])
         o_class = get_obis_entity(data['@type'])
         if o_class:
-            return ns[instance_id], o_class
+            return _get_ns(base_url)[instance_id], o_class
         else:
             return None, None
     else:
         return None, None
 
 
-def iterate_json(data, graph, last_entity=None):
+def iterate_json(data, graph, last_entity=None, base_url=None):
     if isinstance(data, dict):
         # lookup if the id and type in dict result in a ontology entity
-        entity, e_class = create_instance_triple(data)
+        entity, e_class = create_instance_triple(data, base_url=base_url)
         if entity and e_class:
             # if the entity is a Identifier, only create it if it relates to entity previously created
             if e_class in [OBIS.PermanentIdentifier, OBIS.Identifier]:
@@ -102,7 +108,7 @@ def iterate_json(data, graph, last_entity=None):
                         graph.add((entity, obj_prop, Literal(str(prop_value))))
             elif isinstance(value, (dict, list)):
                 # recursively inter over all jason objects
-                iterate_json(value, graph, entity)
+                iterate_json(value, graph, entity, base_url=base_url)
             else:
                 # if its no dict or list test if its kind of object/data/annotation property and set it
                 annotation = get_obis_entity(key)
@@ -119,14 +125,14 @@ def iterate_json(data, graph, last_entity=None):
                 elif entity and annotation and key in ['email']:
                     graph.add((entity, annotation, URIRef("mailto:{}".format(value))))
                 elif entity and annotation and key in ['project', 'space', 'experiment']:
-                    # these json keyword point to integers which relates to other enteties
-                    graph.add((entity, annotation, ns[str(value)]))
+                    # these json keyword point to integers which relates to other entities
+                    graph.add((entity, annotation, _get_ns(base_url)[str(value)]))
                 elif entity and annotation and isinstance(value, str):
                     graph.add((entity, annotation, Literal(value)))
 
     elif isinstance(data, list):
         for item in data:
-            iterate_json(item, graph)
+            iterate_json(item, graph, base_url=base_url)
 
 
 def replace_iris(old: URIRef, new: URIRef, graph: Graph):
@@ -145,19 +151,19 @@ def replace_iris(old: URIRef, new: URIRef, graph: Graph):
         graph.add((triple[0], new, triple[1]))
 
 
-def fix_iris(graph):
+def fix_iris(graph, base_url=None):
     # replace int iris with permids if possible
     for permid in graph[: RDF.type: OBIS.PermanentIdentifier]:
         permid_value = graph.value(permid, RDF.value)
         identifies = graph.value(permid, OBIS.is_identifier_of)
         identifies_type = graph.value(identifies, RDF.type).split('/')[-1].lower()
-        new = ns[f'{identifies_type}/{permid_value}']
+        new = _get_ns(base_url)[f'{identifies_type}/{permid_value}']
         replace_iris(identifies, new, graph)
 
     # replace iri of created object properties with value of code if possible
     for property in graph[: RDF.type: OWL.ObjectProperty]:
         code_value = graph.value(property, OBIS.code)
         if code_value:
-            new = ns[code_value]
+            new = _get_ns(base_url)[code_value]
             replace_iris(property, new, graph)
     return graph

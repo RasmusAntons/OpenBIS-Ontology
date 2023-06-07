416e845a45fbcedd55725626cb9fa5a64a6ff30d
Hanke <hanke@iwm.fraunhofer.de>
2023-06-07 08:26:14 +0200

    added individual tests

--- openbis_json_parser/test.py
+++ openbis_json_parser/test.py
@@ -11,14 +11,41 @@ print(__location__)
 
 
 class TestMain(unittest.TestCase):
-    def test_main(self):
-        with open(os.path.join(__location__, 'tests', 'test_in.json')) as f:
+    def test_user(self):
+        with open(os.path.join(__location__, 'tests', 'user.json')) as f:
             data = json.load(f)
         onto = parse_dict(data)
-        output_file = os.path.join(__location__, 'tests', 'test_out.ttl')
+        output_file = os.path.join(__location__, 'tests', 'user.ttl')
         write_ontology(onto, output_file, target_format='turtle')
         self.assertTrue(os.path.exists(output_file))
-
+    def test_collection(self):
+        with open(os.path.join(__location__, 'tests', 'collection.json')) as f:
+            data = json.load(f)
+        onto = parse_dict(data)
+        output_file = os.path.join(__location__, 'tests', 'collection.ttl')
+        write_ontology(onto, output_file, target_format='turtle')
+        self.assertTrue(os.path.exists(output_file))
+    def test_object(self):
+        with open(os.path.join(__location__, 'tests', 'object.json')) as f:
+            data = json.load(f)
+        onto = parse_dict(data)
+        output_file = os.path.join(__location__, 'tests', 'object.ttl')
+        write_ontology(onto, output_file, target_format='turtle')
+        self.assertTrue(os.path.exists(output_file))
+    def test_project(self):
+        with open(os.path.join(__location__, 'tests', 'project.json')) as f:
+            data = json.load(f)
+        onto = parse_dict(data)
+        output_file = os.path.join(__location__, 'tests', 'project.ttl')
+        write_ontology(onto, output_file, target_format='turtle')
+        self.assertTrue(os.path.exists(output_file))
+    def test_space(self):
+        with open(os.path.join(__location__, 'tests', 'space.json')) as f:
+            data = json.load(f)
+        onto = parse_dict(data)
+        output_file = os.path.join(__location__, 'tests', 'space.ttl')
+        write_ontology(onto, output_file, target_format='turtle')
+        self.assertTrue(os.path.exists(output_file))
 
 if __name__ == '__main__':
     unittest.main()
